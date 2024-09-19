20db16f8029733d88bb5373c1c7b13e6fe9f0c0a
CalumMallorie <calum.mallorie@ed.ac.uk>
2024-09-19 20:42:59 +0100

    Added example template

---
+++ tutorial_example/template/MeshGenerator/CMakeFiles/MeshGenerator.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/calum/biofm_projects/biofm_code/viscoelastic/biofmgroupcode/tools/MeshGenerator/MeshGenerator.cpp" "tools/MeshGenerator/CMakeFiles/MeshGenerator.dir/MeshGenerator.cpp.o" "gcc" "tools/MeshGenerator/CMakeFiles/MeshGenerator.dir/MeshGenerator.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/calum/biofm_projects/biofm_code/viscoelastic/build/lib/geometry/CMakeFiles/geometry.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
